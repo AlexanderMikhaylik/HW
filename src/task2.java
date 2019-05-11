99e119322b666f4918f5e6f9a8a70054c836dc88
mkhlk <alex.mkhlk@gmail.com>
2019-05-11 18:25:34 +0300

    added task2

---
+++ src/task2.java
@@ -0,0 +1,21 @@
+public class task2 {
+
+    public static void main (String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println();
+        System.out.println("int a: " +1);
+        System.out.println("int b: " +2);
+
+        a = a+b-(b=a);
+
+        System.out.println();
+        System.out.println("modified variable a: " +a);
+        System.out.println("modified variable b: " +b);
+
+
+
+    }
+}
