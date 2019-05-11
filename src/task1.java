5458f92078038ae4ccff32e1d9486db0fc52e212
mkhlk <alex.mkhlk@gmail.com>
2019-05-11 15:49:35 +0300

    added task1

---
+++ src/task1.java
@@ -0,0 +1,15 @@
+public class task1 {
+
+
+        public static void main (String[] args) {
+
+
+            String name = "Alexander";
+            int age = 25;
+            Float weight = 65f;
+
+            System.out.printf("name= " + name + " age= " + age + " weight= " + weight);
+
+        }
+    }
+
