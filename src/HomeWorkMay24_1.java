1097a088c9d82f21e852847107246ff5933b4c6d
mkhlk <alex.mkhlk@gmail.com>
2019-05-30 20:55:04 +0300

    Home_work_may_24

---
+++ src/HomeWorkMay24_1.java
@@ -0,0 +1,23 @@
+public class HomeWorkMay24_1 {
+    public static void main (String[] args){
+
+        int x = 3;
+        int y = 4;
+
+        System.out.println(x + y);
+        System.out.print(x + "" + y);
+
+        for (int i = 0; i <= 100; i++){
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        String[] array = {"я", "хочу", "есть"};
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+
+
+    }
+}
